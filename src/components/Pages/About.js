c4600b194b2c4e7c0dd7f8f36cdcb49152c49be7
Luke Morton <lukemorton.dev@gmail.com>
2018-11-05 22:02:14 +0000

    Tidy

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 export default function About ({ changeRoute }) {
   function handleChangeRoute (e) {
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,4 +1,4 @@
-import About from "./About"
-import Home from "./Home"
+import About from './About'
+import Home from './Home'
 
 export default { About, Home }
